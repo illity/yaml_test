18f921583010d96740ca8a4ae4bc30ff952d210e
Arthur Dantas <arthursbdantas@gmail.com>
2023-12-19 12:17:36 -0300

    Update printAsHex.cpp

--- printAsHex.cpp
+++ printAsHex.cpp
@@ -16,6 +16,7 @@ void printAsHex(const char* inputFile) {
         f.close();
     }
     if (size == 0) return;
+    cout << size;
     for (i=0; i<size; i++) {
         cout << (char)(memblock[i]/16+(memblock[i]/16<10?48:55));
         cout << (char)(memblock[i]%16+(memblock[i]%16<10?48:55));
